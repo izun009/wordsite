b2fb66052627fd6f5e9baf30b8346c194be8f388
izzen <izzen@localhost.localdomain>
2022-01-05 07:38:47 +0800

    On going

---
+++ wordsite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ox4r)75caz+xydn$@vaq=j2m%t(2q$0p-=nk=m6z##i9dd5tnx'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
